dace3dd4485796c754d292f51bee102f481069ac
MonicaRiera <monicarigi@hotmail.com>
2014-10-29 12:52:26 +0100

    Order - categories and category items

---
+++ Model/Categories.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace TouristAppV3.Model
+{
+    class Categories : INotifyCollectionChanged
+    {
+        private String _imageUrl;
+        private List<CategoryItemModel> _categoryItems;
+        private ComboBox _categoryItems2;
+        public string Name { get; set; }
+
+        public List<CategoryItemModel> CategoryItems
+        {
+            get { return _categoryItems; }
+            set { _categoryItems = value; }
+        }
+
+        public ComboBox CategBox
+        {
+            get { return _categoryItems2; }
+            set { _categoryItems2 = value;  }
+        }
+
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+
+        public String ImageUrl
+        {
+            get { return _imageUrl; }
+            set { _imageUrl = value; }
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+    }
+}
---
+++ Model/CategoryItemModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TouristAppV3.Annotations;
+
+namespace TouristAppV3.Model
+{
+    class CategoryItemModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _adress;
+        private string _imageUrl;
+        private string _web;
+        private string _email;
+        private string _phone;
+        private string _category;
+
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public String Adress
+        {
+            get { return _adress; }
+            set { _adress = value; }
+        }
+
+        public String ImageUrl
+        {
+            get { return _imageUrl; }
+            set { _imageUrl = value; }
+        }
+
+        public String Web
+        {
+            get { return _web; }
+            set { _web = value; }
+        }
+
+        public String Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public String Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public String Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
